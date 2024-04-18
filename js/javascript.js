b0efc517c969b136d6fc28755aed87da8160333e
felipelolive <felipe.loliveira27@senacsp.edu.br>
2024-04-18 18:33:53 -0300

    aula

--- js/javascript.js
+++ js/javascript.js
@@ -19,12 +19,3 @@ function showSlides() {
   setTimeout(showSlides, 2000); 
 }
 
-           ///nav///
-function clickMenu(){
-  var navbar = document.getElementById("navbar");
-  if (navbar.style.display === "block"){
-      navbar.style.display = "none";
-  }else {
-    navbar.style.display = "block";
-  }
-}
